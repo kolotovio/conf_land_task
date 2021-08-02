876cabf13f39bbe0f39bd31a5ced8c4f0cdb0e6e
kolotovio <kolotov.ian@gmail.com>
2021-08-02 11:50:18 +0300

    add registration notification email w qr-code

---
+++ src/app/Notifications/UserRegistedNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Storage;
+
+class UserRegistedNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Thanks for registration!')
+            ->line('Your registration number is ' . $notifiable->id . '.')
+            ->action('Go to your unique page', url('/' . $notifiable->uuid))
+            ->line('Unique qr-code is attached to this email. Check it below.')
+            ->line('Thank you for using our application!')
+            ->attach(public_path('storage/qrcodes/') . $notifiable->uuid . '.svg', ['as' => 'code.svg', 'mime' => 'image/svg+xml']);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
